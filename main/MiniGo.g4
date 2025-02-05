11ebe9fbdd77090084258aa557407c9915d16097
Lê Nguyễn Gia Hưng <ghung21112004@gmail.com>
2025-02-05 22:42:02 +0700

    15.38% Completed

--- main/MiniGo.g4
+++ main/MiniGo.g4
@@ -25,7 +25,7 @@ options{
 	language = Python3;
 }
 
-// ! -------------------------- Lexical structure ----------------------- ;
+// ! -------------------------- Lexical structure ----------------------- 
 
 // TODO KeyWord
 IF          : 'if' ;
@@ -87,25 +87,43 @@ SEMI       : ';' ;
 ID: [a-zA-Z_][a-zA-Z0-9_]*;
 
 // TODO Literal 
-DECIMAL: '0' | [1-9][0-9]*;
-BINARY: ('0b' [01]+) | ('0B' [01]+);
-OCTAL: ('0o' [0-7]+) | ('0O' [0-7]+);
-HEXADECIMAL: ('0x' [0-9a-fA-F]+) | ('0X' [0-9a-fA-F]+);
+DECIMAL_LIT: '0' | [1-9][0-9]*;
+BINARY_LIT: ('0b' [01]+) | ('0B' [01]+);
+OCTAL_LIT: ('0o' [0-7]+) | ('0O' [0-7]+);
+HEXADECIMAL_LIT: ('0x' [0-9a-fA-F]+) | ('0X' [0-9a-fA-F]+);
 
 fragment EXP: [eE] [+-]? [0-9]+;
-FLOAT: [0-9]+ '.' [0-9]* EXP? | '.' [0-9]+ EXP? | [0-9]+ EXP;
+FLOAT_LIT: [0-9]+ '.' [0-9]* EXP? | '.' [0-9]+ EXP? | [0-9]+ EXP;
 
-STRING: '"' (~["\\] | '\\' ["ntr"\\])* '"';
-BOOLEAN: 'true' | 'false';
-NIL: 'nil';
+STRING_LIT: '"' (~["\\] | '\\' ["ntr"\\])* '"';
+BOOLEAN_LIT: 'true' | 'false';
+NIL_LIT: 'nil';
 
 // TODO SKIP
-COMMENTS: '##' ~[\n]* -> skip; // Comments
-WS: [ \t\r\f\b\n]+ -> skip; // skip spaces, tabs
+SINGLE_LINE_COMMENT: '//' ~[\r\n]* -> skip;
+MULTI_LINE_COMMENT: '/*' (MULTI_LINE_COMMENT | .)*? '*/' -> skip;
+WS: [ \t\r\n\f]+ -> skip;
 
 // TODO ERROR
 ERROR_CHAR: . {raise ErrorToken(self.text)};
+UNCLOSE_STRING: '"' (~["\\] | '\\' ["ntr"\\])* (EOF | '\n')
+{
+    if(len(self.text) >= 2 and self.text[-1] == '\n' and self.text[-2] == '\r'):
+        raise UncloseString(self.text[1:-2])
+    elif (self.text[-1] == '\n'):
+        raise UncloseString(self.text[1:-1])
+    else:
+        raise UncloseString(self.text[1:])
+};
 
+fragment VALID_ESCAPES: 'n' | 't' | 'r' | '"' | '\\';
+
+IllegalEscapeInString: '"' (~["\\] | '\\' ~[VALID_ESCAPES])*
+{
+    for i in range(1, len(self.text)-1):
+        if self.text[i] == '\\' and self.text[i+1] not in VALID_ESCAPES:
+            raise IllegalEscapeInString(self.text[1:i+2])
+};
 //!  -------------------------- end Lexical structure ------------------- //
 
 // //! --------------------------  parser structure ----------------------- //
--- target/main/MiniGoParser.py
+++ target/main/MiniGoParser.py
@@ -11,19 +11,19 @@ else:
 
 def serializedATN():
     with StringIO() as buf:
-        buf.write("\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\3?")
+        buf.write("\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\3J")
         buf.write("/\4\2\t\2\4\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7\t\7\3\2")
         buf.write("\6\2\20\n\2\r\2\16\2\21\3\2\3\2\3\3\3\3\5\3\30\n\3\3\4")
         buf.write("\3\4\3\4\3\4\3\4\3\4\3\5\3\5\3\5\3\5\3\5\3\5\3\6\3\6\3")
         buf.write("\6\3\6\3\6\5\6+\n\6\3\7\3\7\3\7\2\2\b\2\4\6\b\n\f\2\3")
-        buf.write("\3\2\65\66\2+\2\17\3\2\2\2\4\27\3\2\2\2\6\31\3\2\2\2\b")
-        buf.write("\37\3\2\2\2\n*\3\2\2\2\f,\3\2\2\2\16\20\5\4\3\2\17\16")
+        buf.write("\4\2\65\65JJ\2+\2\17\3\2\2\2\4\27\3\2\2\2\6\31\3\2\2\2")
+        buf.write("\b\37\3\2\2\2\n*\3\2\2\2\f,\3\2\2\2\16\20\5\4\3\2\17\16")
         buf.write("\3\2\2\2\20\21\3\2\2\2\21\17\3\2\2\2\21\22\3\2\2\2\22")
         buf.write("\23\3\2\2\2\23\24\7\2\2\3\24\3\3\2\2\2\25\30\5\6\4\2\26")
         buf.write("\30\5\b\5\2\27\25\3\2\2\2\27\26\3\2\2\2\30\5\3\2\2\2\31")
-        buf.write("\32\7\f\2\2\32\33\7\65\2\2\33\34\7:\2\2\34\35\5\n\6\2")
-        buf.write("\35\36\7;\2\2\36\7\3\2\2\2\37 \7<\2\2 !\7=\2\2!\"\5\n")
-        buf.write("\6\2\"#\7>\2\2#$\7;\2\2$\t\3\2\2\2%&\5\f\7\2&\'\7?\2\2")
+        buf.write("\32\7\f\2\2\32\33\7\65\2\2\33\34\7D\2\2\34\35\5\n\6\2")
+        buf.write("\35\36\7E\2\2\36\7\3\2\2\2\37 \7F\2\2 !\7G\2\2!\"\5\n")
+        buf.write("\6\2\"#\7H\2\2#$\7E\2\2$\t\3\2\2\2%&\5\f\7\2&\'\7I\2\2")
         buf.write("\'(\5\n\6\2(+\3\2\2\2)+\5\f\7\2*%\3\2\2\2*)\3\2\2\2+\13")
         buf.write("\3\2\2\2,-\t\2\2\2-\r\3\2\2\2\5\21\27*")
         return buf.getvalue()
@@ -42,7 +42,7 @@ class MiniGoParser ( Parser ):
     literalNames = [ "<INVALID>", "'if'", "'else'", "'for'", "'return'", 
                      "'func'", "'type'", "'struct'", "'interface'", "'string'", 
                      "'int'", "'float'", "'boolean'", "'const'", "'var'", 
-                     "'continue'", "'break'", "'range'", "'nil'", "'true'", 
+                     "'continue'", "'break'", "'range'", "<INVALID>", "'true'", 
                      "'false'", "'+'", "'-'", "'*'", "'/'", "'%'", "'=='", 
                      "'!='", "'<='", "'>='", "'<'", "'>'", "'&&'", "'||'", 
                      "'!'", "'+='", "'-='", "'*='", "'/='", "'%='", "':='", 
@@ -57,9 +57,12 @@ class MiniGoParser ( Parser ):
                       "AND", "OR", "NOT", "PLUSEQ", "MINUSEQ", "MULEQ", 
                       "DIVEQ", "MODEQ", "COLONEQ", "ASSIGN", "DOT", "LPAREN", 
                       "RPAREN", "LBRACE", "RBRACE", "LBRACK", "RBRACK", 
-                      "COMMA", "SEMI", "ID", "INT_LIT", "COMMENTS", "WS", 
-                      "ERROR_CHAR", "ASSIGNI", "COMCOMMA", "PRINT", "LP", 
-                      "RP", "ADD" ]
+                      "COMMA", "SEMI", "ID", "DECIMAL_LIT", "BINARY_LIT", 
+                      "OCTAL_LIT", "HEXADECIMAL_LIT", "FLOAT_LIT", "STRING_LIT", 
+                      "BOOLEAN_LIT", "NIL_LIT", "SINGLE_LINE_COMMENT", "MULTI_LINE_COMMENT", 
+                      "WS", "ERROR_CHAR", "UNCLOSE_STRING", "IllegalEscapeInString", 
+                      "ASSIGNI", "COMCOMMA", "PRINT", "LP", "RP", "ADD", 
+                      "INT_LIT" ]
 
     RULE_program = 0
     RULE_statement = 1
@@ -123,16 +126,27 @@ class MiniGoParser ( Parser ):
     COMMA=49
     SEMI=50
     ID=51
-    INT_LIT=52
-    COMMENTS=53
-    WS=54
-    ERROR_CHAR=55
-    ASSIGNI=56
-    COMCOMMA=57
-    PRINT=58
-    LP=59
-    RP=60
-    ADD=61
+    DECIMAL_LIT=52
+    BINARY_LIT=53
+    OCTAL_LIT=54
+    HEXADECIMAL_LIT=55
+    FLOAT_LIT=56
+    STRING_LIT=57
+    BOOLEAN_LIT=58
+    NIL_LIT=59
+    SINGLE_LINE_COMMENT=60
+    MULTI_LINE_COMMENT=61
+    WS=62
+    ERROR_CHAR=63
+    UNCLOSE_STRING=64
+    IllegalEscapeInString=65
+    ASSIGNI=66
+    COMCOMMA=67
+    PRINT=68
+    LP=69
+    RP=70
+    ADD=71
+    INT_LIT=72
 
     def __init__(self, input:TokenStream, output:TextIO = sys.stdout):
         super().__init__(input, output)
